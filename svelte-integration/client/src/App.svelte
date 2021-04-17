33a1078bd4bcfa3e81845774d24c2b72e31be527
Avishekh Sinha <avishekh_sinha@optum.com>
2021-04-17 20:24:03 +0530

    Svelte MPA integration

---
+++ svelte-integration/client/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let name;
+	export let age
+	function handleClick(){
+		console.log("Click");
+		sessionStorage.setItem("test","TestValue");
+	}
+</script>
+
+<main>
+	<h1>Hello {name}! age is {age}</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button on:click={handleClick}>Click</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
